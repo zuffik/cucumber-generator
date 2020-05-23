1cbfdeda24a13c365d4d1d07dc2515016ce4745c
Kristián Žuffa <kristian.zuffa@gmail.com>
2020-05-23 20:09:40 +0200

    executable

---
+++ make-executable.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+const file = './dist/index.js';
+const content = fs.readFileSync(file).toString();
+fs.writeFileSync(file, '#!/usr/bin/env node\n\n' + content)
+fs.chmodSync(file, 755);
