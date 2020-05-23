2442b1e9d851511d435da7ce21f4bd3748ac05fd
Kristián Žuffa <kristian.zuffa@gmail.com>
2020-05-23 20:11:16 +0200

    fixed args

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { runner } from './cli/Runner';
-import { args } from './cli/Args';
 
 export * from './generators/Generator';
 export * from './generators/TemplateGenerator';
 
 if (require.main === module) {
-  runner(args).catch((e) => console.error(e));
+  runner(require('./cli/Args')).catch((e) => console.error(e));
 }
